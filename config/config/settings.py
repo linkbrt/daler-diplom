e0c9786b969189ac73d8e699da718157ec025f3f
Dan <zhulkin01@gmail.com>
2023-06-13 23:49:46 +0300

    gd

--- config/config/settings.py
+++ config/config/settings.py
@@ -42,9 +42,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:3000',]
 
